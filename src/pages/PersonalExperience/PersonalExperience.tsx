08139260c72630ea6057c363ce7d34820829552e
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-02-10 01:41:59 -0400

    adding video link

--- src/pages/PersonalExperience/PersonalExperience.tsx
+++ src/pages/PersonalExperience/PersonalExperience.tsx
@@ -7,14 +7,14 @@ const PersonalExperience: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle style={{textAlign: "center"}}>Experiencia Personal</IonTitle>
+          <IonTitle style={{ textAlign: "center" }}>Experiencia Personal</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <div className="videoContainer">
           <h2>Experiencia Personal Creando la App</h2>
           <div>
-            <iframe width="720" height="400" src="https://www.youtube.com/embed/noVideo" title="DONATY SE LA MONTA EN SU CARA A PAPERA POR ACUSARLO DE ATRACO" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            <iframe width="704" height="396" src="https://www.youtube.com/embed/vaGlVJCoy9E" title="EXPERIENCIA UTILIZANDO IONIC POR PRIMERA VEZ - CRISTIAN SANCHEZ" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
           </div>
         </div>
       </IonContent>
